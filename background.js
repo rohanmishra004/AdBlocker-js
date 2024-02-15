1a72fe4c66f681d712ee9ec6bd4af7c9be92b7b1
Rohan Mishra <rohanmishra.004@gmail.com>
2024-02-15 11:01:29 +0530

    Add Blocker

---
+++ background.js
@@ -0,0 +1,19 @@
+chrome.webRequest.onBeforeRequest.addListener(
+    function (details) { return { cancel: true } },
+    { urls: defaultFilters },
+    ["blocking"]
+)
+
+const defaultFilters = [
+    "*://*.doubleclick.net/*",
+    "*://partner.googleadservices.com/*",
+    "*://*.googlesyndication.com/*",
+    "*://*.google-analytics.com/*",
+    "*://creative.ak.fbcdn.net/*",
+    "*://*.adbrite.com/*",
+    "*://*.exponential.com/*",
+    "*://*.quantserve.com/*",
+    "*://*.scorecardresearch.com/*",
+    "*://*.zedo.com/*",
+    "https://www.facebook.com/",
+]
